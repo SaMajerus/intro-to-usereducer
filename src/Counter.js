ce108ee1b455c324fe25a9ef714adb699d05c53f
Samuel Majerus <ladolego@gmail.com>
2022-12-12 11:42:26 -0800

    Replaces 'useState()' with 'useReducer()'.   [End of code-along work for Lsn 4 (see link).] [https://www.learnhowtoprogram.com/react/react-with-apis/the-usereducer-hook]

--- src/Counter.js
+++ src/Counter.js
@@ -16,17 +16,17 @@ function reducer(state, action) {
 }
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [hidden, setHidden] = useState(false);
 
   useEffect(() => {
-    document.title = counter;
-  }, [counter]);
+    document.title = state.counter;
+  }, [state.counter]);
 
   return (
     <React.Fragment>
-      {hidden ? <h1>Count Hidden</h1> : <h1>{counter}</h1>}
-      <button onClick={() => setCounter(counter + 1)}>Count!</button>
+      {hidden ? <h1>Count Hidden</h1> : <h1>{state.counter}</h1>}
+      <button onClick={() => dispatch({type: 'increment'})}>Count!</button>
       <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
     </React.Fragment>
   );
